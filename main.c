b82e7e3b7ee6bf1b7d9adcbfbd57fd42d6275cfc
ChaeyunK <iehate52@gmail.com>
2023-10-09 16:59:40 +0900

    6

--- main.c
+++ main.c
@@ -4,23 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-
-	unsigned int x;
-	int b;
 	
-	printf("input a number : ");
-	scanf("%ui", &x);
+	int a;
 	
-	for(b=0; x != 0; x>>=1)
-	{
-		if(x&1)
-		{
-			b++;
-		}
-	}
+	printf("Input second :");
+	scanf("%i", &a);
 	
-	printf("The result is : %i\n", b);
+	printf("%i seconds is %i:%i:%i", a, a/3600, (a%3600)/60, a%60);
 	
-	
-	return 0;
+ 
+ 	system("PAUSE");
+ 	return 0;
 }
+
+	
